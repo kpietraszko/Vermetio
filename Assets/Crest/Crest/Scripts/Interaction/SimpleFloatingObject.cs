73ab78e1202b18830ef4015e006fa937dbddd72a
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-07-31 16:44:48 +0200

    Added tooltips

--- Assets/Crest/Crest/Scripts/Interaction/SimpleFloatingObject.cs
+++ Assets/Crest/Crest/Scripts/Interaction/SimpleFloatingObject.cs
@@ -113,12 +113,12 @@ namespace Crest
             var buoyancy = -Physics.gravity.normalized * _buoyancyCoeff * bottomDepth * bottomDepth * bottomDepth;
             _rb.AddForce(buoyancy, ForceMode.Acceleration);
 
-            // // Approximate hydrodynamics of sliding along water
-            // if (_accelerateDownhill > 0f)
-            // {
-            //     _rb.AddForce(new Vector3(normal.x, 0f, normal.z) * -Physics.gravity.y * _accelerateDownhill, ForceMode.Acceleration);
-            // }
-            //
+            // Approximate hydrodynamics of sliding along water
+            if (_accelerateDownhill > 0f)
+            {
+                _rb.AddForce(new Vector3(normal.x, 0f, normal.z) * -Physics.gravity.y * _accelerateDownhill, ForceMode.Acceleration);
+            }
+            
             // Apply drag relative to water
             var forcePosition = _rb.position + _forceHeightOffset * Vector3.up;
             _rb.AddForceAtPosition(Vector3.up * Vector3.Dot(Vector3.up, -velocityRelativeToWater) * _dragInWaterUp, forcePosition, ForceMode.Acceleration);

229d911e51d17c5e0d571c4476b83c05168dadb8
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-08-15 23:05:36 +0200

    Fixed angle between current and target heading

--- Assets/Scripts/BoatEngineSystem.cs
+++ Assets/Scripts/BoatEngineSystem.cs
@@ -41,6 +41,39 @@ public class BoatEngineSystem : SystemBase
             var forcePoint = math.transform(localToWorld.Value, pm.Transform.pos);
             forcePoint.DrawCross(1f, Color.black, deltaTime);
             pv.ApplyImpulse(impulseMass, translation, rotation, impulse, translation.Value); // is point correct?
+            // Debug.Log($"Applying impulse {impulse}");
+
+            var rotationAxis = math.normalizesafe(localToWorld.Up + probyBuoyant.TurningHeel * localToWorld.Forward);
+            // var rotationToTarget = Math.FromToRotation(localToWorld.Forward, targetHeading.Value);
+            var angleToTargetInRad = SignedAngle(localToWorld.Forward, input.TargetDirection, new float3(0,1,0));
+            Debug.Log($"{angleToTargetInRad}");
+            Debug.DrawLine(translation.Value, translation.Value + localToWorld.Forward * 10f, Color.red, deltaTime);
+            Debug.DrawLine(translation.Value, translation.Value + input.TargetDirection * 10f, Color.yellow, deltaTime);
+
+            pv.ApplyAngularImpulse(impulseMass, rotationAxis * math.clamp(angleToTargetInRad, -10f, 10f) * probyBuoyant.TurnPower * deltaTime);
         }).Schedule();
+    }
+
+    // Returns the angle in degrees between /from/ and /to/. This is always the smallest
+    private static float Angle(float3 from, float3 to)
+    {
+        const float unityEpsilonNormalSqrt = 1e-15F;
+        // sqrt(a) * sqrt(b) = sqrt(a * b) -- valid for real numbers
+        var denominator = math.sqrt(math.lengthsq(from) * math.lengthsq(to));
+        if (denominator < unityEpsilonNormalSqrt)
+            return 0F;
+
+        var dot = math.clamp(math.dot(from, to) / denominator, -1F, 1F);
+        return math.degrees(math.acos(dot));
+    }
+
+    // The smaller of the two possible angles between the two vectors is returned, therefore the result will never be greater than 180 degrees or smaller than -180 degrees.
+    // If you imagine the from and to vectors as lines on a piece of paper, both originating from the same point, then the /axis/ vector would point up out of the paper.
+    // The measured angle between the two vectors would be positive in a clockwise direction and negative in an anti-clockwise direction.
+    private static float SignedAngle(float3 from, float3 to, float3 axis)
+    {
+        var unsignedAngle = Angle(from, to);
+        var sign = math.sign(math.dot(math.cross(from, to), axis));
+        return unsignedAngle * sign;
     }
 }

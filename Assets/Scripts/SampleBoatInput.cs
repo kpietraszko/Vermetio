5e1dc1499bfcccdfec0dda4aa72534d20f90fb42
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-08-26 18:58:58 +0200

    Working direction line that only appears with throttle; Boat foam

--- Assets/Scripts/SampleBoatInput.cs
+++ Assets/Scripts/SampleBoatInput.cs
@@ -1,9 +1,13 @@
+using E7.ECS.LineRenderer;
+using Unity.Assertions;
 using Unity.Burst;
 using Unity.Collections;
+using Unity.Core;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.NetCode;
+using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -18,8 +22,8 @@ public class SampleBoatInput : SystemBase
     
     protected override void OnUpdate()
     {
-        var localInput = GetSingleton<CommandTargetComponent>().targetEntity;
-        if (localInput == Entity.Null)
+        var localInputEntity = GetSingleton<CommandTargetComponent>().targetEntity;
+        if (localInputEntity == Entity.Null)
         {
             AddKeyboardInputBuffer();
             return;
@@ -33,30 +37,53 @@ public class SampleBoatInput : SystemBase
         if (keyboard == null) // somehow user has no keyboard
             return;
 
-        var targetHeading = EntityManager.GetComponentData<LocalToWorld>(localInput).Forward;
-        if (EntityManager.HasComponent<BoatTargetHeadingComponent>(localInput))
-            targetHeading = EntityManager.GetComponentData<BoatTargetHeadingComponent>(localInput).Value;
-        else EntityManager.AddComponent<BoatTargetHeadingComponent>(localInput);
+        var targetHeading = EntityManager.GetComponentData<LocalToWorld>(localInputEntity).Forward;
+        if (EntityManager.HasComponent<BoatTargetHeadingComponent>(localInputEntity))
+        {
+            var existingHeading = EntityManager.GetComponentData<BoatTargetHeadingComponent>(localInputEntity).Value;
+            targetHeading = math.all(existingHeading == default) ? targetHeading : existingHeading;
+        }
+        else EntityManager.AddComponent<BoatTargetHeadingComponent>(localInputEntity);
 
         var rotationDirection = (keyboard.aKey.isPressed ? -1f : 0) + (keyboard.dKey.isPressed ? 1f : 0);
         var angleToRotateHeadingBy = math.PI / 2f * Time.DeltaTime * rotationDirection; // 90 degrees per second
         var rotationOfHeading = quaternion.AxisAngle(new float3(0, 1, 0), angleToRotateHeadingBy);
-        targetHeading = math.normalize(math.mul(rotationOfHeading, targetHeading));
+        targetHeading = math.normalizesafe(math.mul(rotationOfHeading, targetHeading));
 
         input.TargetDirection = targetHeading;
-        EntityManager.SetComponentData(localInput, new BoatTargetHeadingComponent() { Value = targetHeading });
+        EntityManager.SetComponentData(localInputEntity, new BoatTargetHeadingComponent() { Value = targetHeading });
         
         var throttleInput = (keyboard.wKey.isPressed ? 1f : 0) + (keyboard.sKey.isPressed ? -1f : 0);
         input.Throttle = throttleInput;
         
-        var playerPosition = EntityManager.GetComponentData<Translation>(localInput).Value;
-        Debug.DrawLine(playerPosition, playerPosition + targetHeading * 10f * throttleInput, Color.green, Time.DeltaTime);
+        var playerPosition = EntityManager.GetComponentData<Translation>(localInputEntity).Value;
+        Debug.DrawLine(playerPosition, playerPosition + targetHeading * 4, Color.black, Time.DeltaTime);
 
         // if (keyboard.aKey.isPressed)
         //     input
         
-        var inputBuffer = EntityManager.GetBuffer<BoatKeyboardInput>(localInput);
+        var inputBuffer = EntityManager.GetBuffer<BoatKeyboardInput>(localInputEntity);
         inputBuffer.AddCommandData(input);
+        
+        // Direction line
+        Entities.WithStructuralChanges().ForEach((Entity entity, ref LineSegment lineSegment, in Parent parent) =>
+        {
+            if (parent.Value == localInputEntity)
+                lineSegment = new LineSegment(playerPosition + targetHeading * 12f, playerPosition + targetHeading * 18f, lineSegment.lineWidth);
+
+            var isHidden = HasComponent<DisableRendering>(entity);
+            if (math.abs(throttleInput) > 0.001f && isHidden)
+            {
+                EntityManager.RemoveComponent<DisableRendering>(entity);
+                Debug.Log("Unhiding");
+            }
+
+            if (math.abs(throttleInput) < 0.001 && !isHidden)
+            {
+                EntityManager.AddComponent<DisableRendering>(entity);
+                Debug.Log("Hiding");
+            }
+        }).Run();
     }
 
     private void AddKeyboardInputBuffer()

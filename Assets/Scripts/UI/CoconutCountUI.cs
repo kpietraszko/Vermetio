75c16c9efe868f66f3a9a3b20389f3f3d744406c
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-09-16 16:34:06 +0200

    Basic coconut count UI that updates from inventory every 7 frames

--- Assets/Scripts/UI/CoconutCountUI.cs
+++ Assets/Scripts/UI/CoconutCountUI.cs
@@ -1,53 +1,65 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using Unity.Entities;
-// using Unity.NetCode;
-// using Unity.NetCode.Editor;
-// using UnityEngine;
-// using UnityEngine.UIElements;
-// using Vermetio;
-//
-// #if UNITY_EDITOR || UNITY_CLIENT
-// public class CoconutCountUI : MonoBehaviour
-// {
-//     // private Label _coconutCountLabel;
-//
-//     void OnEnable()
-//     {
-//     }
-//
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//     }
-//
-//     private static int? GetCoconutCountFromInventory()
-//     {
-//         var world =
-//             #if UNITY_EDITOR
-//                 MultiplayerPlayModeControllerSystem.PresentedClient.World;
-//             #else
-//                 EntityHelpers.GetWorldWith<ClientSimulationSystemGroup>(World.All);
-//             #endif
-//
-//         if (world == null)
-//             return null;
-//
-//         var networkIdQuery = world.EntityManager.CreateEntityQuery(typeof(NetworkIdComponent));
-//         if (networkIdQuery.CalculateEntityCount() != 1)
-//             return null;
-//
-//         var playerEntity = world.EntityManager.GetComponentData<CommandTargetComponent>(networkIdQuery.GetSingletonEntity())
-//             .targetEntity;
-//         var inventory = world.EntityManager.GetComponentData<PlayerInventoryComponent>(playerEntity);
-//         return inventory.Coconuts;
-//     }
-//
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         
-//     }
-// }
-// #endif
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
+using TMPro;
+using Unity.Entities;
+using Unity.NetCode;
+using Unity.NetCode.Editor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+#if UNITY_EDITOR || UNITY_CLIENT
+namespace Vermetio.Client
+{
+    public class CoconutCountUI : MonoBehaviour
+    {
+        private TMP_Text _coconutCountLabel;
+
+        void OnEnable()
+        {
+            _coconutCountLabel = GetComponent<TMP_Text>();
+        }
+
+        // Start is called before the first frame update
+        async UniTaskVoid Start()
+        {
+            Debug.Log("Start called");
+            await foreach(var _ in UniTaskAsyncEnumerable.IntervalFrame(7, PlayerLoopTiming.PreLateUpdate))
+            {
+                var count = GetCoconutCountFromInventory();
+                if (count == null)
+                    continue;
+
+                _coconutCountLabel.text = count.ToString();
+            }
+        }
+
+        private static int? GetCoconutCountFromInventory()
+        {
+            var world =
+#if UNITY_EDITOR
+                MultiplayerPlayModeControllerSystem.PresentedClient?.World;
+#else
+                EntityHelpers.GetWorldWith<ClientSimulationSystemGroup>(World.All);
+#endif
+
+            if (world == null)
+                return null;
+
+            var networkIdQuery = world.EntityManager.CreateEntityQuery(typeof(NetworkIdComponent));
+            if (networkIdQuery.CalculateEntityCount() != 1)
+                return null;
+
+            var playerEntity = world.EntityManager
+                .GetComponentData<CommandTargetComponent>(networkIdQuery.GetSingletonEntity()).targetEntity;
+            if (!world.EntityManager.HasComponent<PlayerInventoryComponent>(playerEntity))
+                return null;
+
+            var inventory = world.EntityManager.GetComponentData<PlayerInventoryComponent>(playerEntity);
+            return inventory.Coconuts;
+        }
+    }
+}
+#endif

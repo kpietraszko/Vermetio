75d1742ed3cb1da57ec89f09a7c835035f61f36c
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-10-03 22:07:08 +0200

    Update buildServer.ps1

--- BuildScripts/buildServer.ps1
+++ BuildScripts/buildServer.ps1
@@ -1,3 +1,2 @@
 $ErrorView = "NormalView"
-cd "C:\Program Files\Unity\2020.3.19f1\Editor"
-.\Unity.exe -batchmode -quit -nographics -project Vermetio -executeMethod BuildRunner.BuildServer
+C:\Program Files\Unity\2020.3.19f1\Editor\Unity -batchmode -quit -nographics -project Vermetio -executeMethod BuildRunner.BuildServer

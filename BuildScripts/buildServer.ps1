43d9008b924b26fe580bda6f36c0c25a1fe2b8f7
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-10-04 13:29:19 +0200

    Update buildServer.ps1

--- BuildScripts/buildServer.ps1
+++ BuildScripts/buildServer.ps1
@@ -1,4 +1,2 @@
 $ErrorView = "NormalView"
-echo $env:GITHUB_ACTION_PATH
-cd "C:\Program Files\Unity\2020.3.19f1\Editor"
-.\Unity.exe -batchmode -quit -nographics -projectPath .. -executeMethod BuildRunner.BuildServer
+C:\Program` Files\Unity\2020.3.19f1\Editor\Unity.exe -batchmode -quit -nographics -projectPath . -executeMethod BuildRunner.BuildServer

4ec6fc566bcfe366a68710891aab89d31e18660d
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-10-04 13:04:58 +0200

    fix project path

--- BuildScripts/buildServer.ps1
+++ BuildScripts/buildServer.ps1
@@ -1,3 +1,3 @@
 $ErrorView = "NormalView"
 cd "C:\Program Files\Unity\2020.3.19f1\Editor"
-.\Unity.exe -batchmode -quit -nographics -project Vermetio -executeMethod BuildRunner.BuildServer
+.\Unity.exe -batchmode -quit -nographics -projectPath .. -executeMethod BuildRunner.BuildServer

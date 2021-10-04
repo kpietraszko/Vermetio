b4fafe1a890a14774424ac224b3ddc30f65a6e33
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-10-04 13:17:47 +0200

    print action path

--- BuildScripts/buildServer.ps1
+++ BuildScripts/buildServer.ps1
@@ -1,3 +1,4 @@
 $ErrorView = "NormalView"
+echo $env:GITHUB_ACTION_PATH
 cd "C:\Program Files\Unity\2020.3.19f1\Editor"
 .\Unity.exe -batchmode -quit -nographics -projectPath .. -executeMethod BuildRunner.BuildServer

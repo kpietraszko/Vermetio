007261c1f13902624fcdeac10535090584581c53
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-10-04 14:32:52 +0200

    wait for unity build

--- BuildScripts/buildServer.ps1
+++ BuildScripts/buildServer.ps1
@@ -1,2 +1,2 @@
 $ErrorView = "NormalView"
-C:\Program` Files\Unity\2020.3.19f1\Editor\Unity.exe -batchmode -quit -nographics -projectPath .. -executeMethod BuildRunner.BuildServer
+C:\Program` Files\Unity\2020.3.19f1\Editor\Unity.exe -batchmode -quit -nographics -projectPath .. -executeMethod BuildRunner.BuildServer | Out-String

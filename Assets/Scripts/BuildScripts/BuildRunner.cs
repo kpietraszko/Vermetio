eccac8400a0727098a1de73d50705d721b5282e2
kpietraszko <krzysztof@pietraszko.cba.pl>
2021-10-04 15:02:48 +0200

    Change build folder name - conflicted with gitignore

---
+++ Assets/Scripts/BuildScripts/BuildRunner.cs
@@ -0,0 +1,20 @@
+using Unity.Build;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildRunner
+{
+    public static void BuildServer()
+    {
+        var bc = AssetDatabase.LoadAssetAtPath<BuildConfiguration>(
+            "BuildSettings/WindowsClassicBuildConfiguration.buildconfiguration");
+        var result = bc.Build(); //This will build your game
+    }
+
+    public static void BuildClient()
+    {
+        var bc = AssetDatabase.LoadAssetAtPath<BuildConfiguration>(
+            "Assets/BuildSettings/WindowsClassicBuildConfigurationClient.buildconfiguration");
+        var result = bc.Build(); //This will build your game
+    }
+}
